bdfdbb30c4d1854d950d0c207faa63c2b2ce087c
LDZ1704 <lamn9049@gmail.com>
2025-11-13 20:17:59 +0700

    create project asp.net

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AI_Voice_Translator_SaaS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
