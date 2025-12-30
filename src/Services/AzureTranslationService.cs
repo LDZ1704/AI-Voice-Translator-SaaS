d95232e73ed117c5000f1341c6bf8ca431f5cbf9
LDZ1704 <lamn9049@gmail.com>
2025-12-30 14:31:06 +0700

    fix Unauthorize in Translation

--- src/Services/AzureTranslationService.cs
+++ src/Services/AzureTranslationService.cs
@@ -1,5 +1,4 @@
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using AI_Voice_Translator_SaaS.Interfaces;
@@ -49,30 +48,7 @@ namespace AI_Voice_Translator_SaaS.Services
 
                 _logger.LogInformation("AzureTranslator cache miss - calling Azure Translator API");
 
-                var baseUrl = _endpoint.TrimEnd('/');
-                string url;
-
-                if (baseUrl.Contains("cognitiveservices.azure.com"))
-                {
-                    if (!baseUrl.Contains("/translator/text/"))
-                    {
-                        url = $"{baseUrl}/translator/text/v3.0/translate?from={sourceLanguage}&to={targetLanguage}";
-                    }
-                    else
-                    {
-                        url = $"{baseUrl}/v3.0/translate?from={sourceLanguage}&to={targetLanguage}";
-                    }
-                }
-                else if (baseUrl.StartsWith("http://") || baseUrl.StartsWith("https://"))
-                {
-                    url = $"{baseUrl}/translate?api-version=3.0&from={sourceLanguage}&to={targetLanguage}";
-                }
-                else
-                {
-                    url = $"https://{baseUrl}/translate?api-version=3.0&from={sourceLanguage}&to={targetLanguage}";
-                }
-
-                _logger.LogInformation("Azure Translator URL: {Url}", url);
+                string route = $"/translate?api-version=3.0&from={sourceLanguage}&to={targetLanguage}";
 
                 var requestBody = new[]
                 {
@@ -80,24 +56,23 @@ namespace AI_Voice_Translator_SaaS.Services
                 };
 
                 var json = JsonSerializer.Serialize(requestBody);
-                using var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-                var request = new HttpRequestMessage(HttpMethod.Post, url)
-                {
-                    Content = content
-                };
-
+                using var request = new HttpRequestMessage();
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(_endpoint + route);
+                request.Content = new StringContent(json, Encoding.UTF8, "application/json");
                 request.Headers.Add("Ocp-Apim-Subscription-Key", _subscriptionKey);
                 request.Headers.Add("Ocp-Apim-Subscription-Region", _region);
 
-                using var response = await _httpClient.PostAsync(url, content);
+                _logger.LogInformation("Azure Translator Request: {Url}", request.RequestUri);
+
+                using var response = await _httpClient.SendAsync(request).ConfigureAwait(false);
                 var responseString = await response.Content.ReadAsStringAsync();
 
                 if (!response.IsSuccessStatusCode)
                 {
                     _logger.LogError("Azure Translator error: {StatusCode} - {Body}", response.StatusCode, responseString);
-                    _logger.LogInformation("Translator config: Endpoint={_endpoint}, Region={_region}, KeyExists={HasKey}", _endpoint, _region, !string.IsNullOrEmpty(_subscriptionKey));
-                    return (false, $"Azure Translator error: {response.StatusCode}");
+                    return (false, $"Azure Translator error: {response.StatusCode} - {responseString}");
                 }
 
                 using var doc = JsonDocument.Parse(responseString);
@@ -131,5 +106,3 @@ namespace AI_Voice_Translator_SaaS.Services
         }
     }
 }
-
-
